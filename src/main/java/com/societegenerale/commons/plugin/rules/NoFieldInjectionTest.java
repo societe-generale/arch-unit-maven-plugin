9f102c0829ae99b688b3759d516f28aafcb3d67e
Vincent FUCHS <vincent.fuchs@gmail.com>
2019-04-01 08:29:23 +0530

    rules should apply only on main code, not on test code

--- src/main/java/com/societegenerale/commons/plugin/rules/NoFieldInjectionTest.java
+++ src/main/java/com/societegenerale/commons/plugin/rules/NoFieldInjectionTest.java
@@ -1,5 +1,7 @@
 package com.societegenerale.commons.plugin.rules;
 
+import com.societegenerale.commons.plugin.utils.ArchUtils;
+
 import javax.inject.Inject;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noFields;
@@ -8,6 +10,7 @@ public class NoFieldInjectionTest implements ArchRuleTest  {
 
     @Override
     public void execute(String path) {
-        noFields().should().beAnnotatedWith(Inject.class);
+
+        noFields().should().beAnnotatedWith(Inject.class).check(ArchUtils.importAllClassesInPackage(path, ArchUtils.SRC_CLASSES_FOLDER));
     }
 }
