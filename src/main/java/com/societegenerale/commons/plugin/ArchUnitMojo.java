99d4dc2a0ebc28c26727b0d3652dac392f489046
agarg020917 <arpit.garg@socgen.com>
2019-05-08 09:38:51 +0530

    code refactored

--- src/main/java/com/societegenerale/commons/plugin/ArchUnitMojo.java
+++ src/main/java/com/societegenerale/commons/plugin/ArchUnitMojo.java
@@ -19,7 +19,6 @@ import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
