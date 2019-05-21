fb6183bef832a33bfecb681d41712a25e3a7edc9
Fanon JUPKWO <jupsfan@gmail.com>
2019-05-21 13:14:25 +0200

    New rule added : no java util date (#5)

--- src/test/java/com/societegenerale/commons/plugin/rules/ArchUtilsTest.java
+++ src/test/java/com/societegenerale/commons/plugin/rules/ArchUtilsTest.java
@@ -1,54 +1,55 @@
 package com.societegenerale.commons.plugin.rules;
 
-import com.societegenerale.commons.plugin.utils.ArchUtils;
-import com.tngtech.archunit.core.domain.JavaClass;
-import com.tngtech.archunit.core.domain.JavaClasses;
-import org.junit.Assert;
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.societegenerale.commons.plugin.utils.ArchUtils;
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaClasses;
 
 public class ArchUtilsTest {
 
+	@Test
+	public void constructorInvocationTest() {
+		try {
+			final Constructor<ArchUtils> c = ArchUtils.class.getDeclaredConstructor();
+			c.setAccessible(true);
+			final ArchUtils newInstance = c.newInstance();
+			Assert.assertNull(newInstance);
+		} catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException e) {
+			assertThat(e).hasCauseExactlyInstanceOf(UnsupportedOperationException.class);
+		}
+	}
 
-  @Test
-  public void constructorInvocationTest() {
-    try {
-      final Constructor<ArchUtils> c = ArchUtils.class.getDeclaredConstructor();
-      c.setAccessible(true);
-      final ArchUtils newInstance = c.newInstance();
-      Assert.assertNull(newInstance);
-    } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException
-        | InvocationTargetException e) {
-      assertThat(e).hasCauseExactlyInstanceOf(UnsupportedOperationException.class);
-    }
-  }
+	@Test
+	public void testNumberOfRulesAvailable() {
+		JavaClasses classes = ArchUtils
+				.importAllClassesInPackage("./target/classes/com/societegenerale/commons/plugin/", "rules");
+		int noOfClasses = 0;
+		for (JavaClass javaClass : classes) {
+			if (!javaClass.getName().contains("$") && !javaClass.isInterface()) {
+				noOfClasses++;
+			}
+		}
+		assertThat(noOfClasses).isEqualTo(10);
+	}
 
-  @Test
-  public void testNumberOfRulesAvailable() {
-    JavaClasses classes = ArchUtils.importAllClassesInPackage("./target/classes/com/societegenerale/commons/plugin/", "rules");
-    int noOfClasses = 0;
-    for (JavaClass javaClass : classes) {
-      if(! javaClass.getName().contains("$") && !javaClass.isInterface()) {
-        noOfClasses++;
-      }
-    }
-    assertThat(noOfClasses).isEqualTo(9);
-  }
-
-  @Test
-  public void totalClassesInPathWhenPackageFolderDoesNotExists() {
-    JavaClasses classes = ArchUtils.importAllClassesInPackage("./target/classes", "classFolder");
-    int noOfClasses = 0;
-    for (JavaClass javaClass : classes) {
-      if(!javaClass.getName().contains("$")) {
-        noOfClasses++;
-      }
-    }
-    assertThat(noOfClasses).isEqualTo(16);
-  }
+	@Test
+	public void totalClassesInPathWhenPackageFolderDoesNotExists() {
+		JavaClasses classes = ArchUtils.importAllClassesInPackage("./target/classes", "classFolder");
+		int noOfClasses = 0;
+		for (JavaClass javaClass : classes) {
+			if (!javaClass.getName().contains("$")) {
+				noOfClasses++;
+			}
+		}
+		assertThat(noOfClasses).isEqualTo(17);
+	}
 
 }
