220e0572ded370ec91b2590bdea13808ac1b1148
FanJups <jupsfan@gmail.com>
2019-07-09 14:37:28 +0200

    Improving coveralls test  8
    2 changes :
    
    1- I reduce the lines  of  NoPublicFieldRuleTest.java (use of ArchUnit API)  and  NoPublicFieldRuleTestTest.java .
    
    2- I created public static JavaClasses importAllClassesInPackage(String path) using polymorphism in ArchUtils.java

--- src/main/java/com/societegenerale/commons/plugin/utils/ArchUtils.java
+++ src/main/java/com/societegenerale/commons/plugin/utils/ArchUtils.java
@@ -1,35 +1,34 @@
 package com.societegenerale.commons.plugin.utils;
 
-import com.societegenerale.commons.plugin.model.ConfigurableRule;
-import com.tngtech.archunit.core.domain.JavaClass;
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.lang.ArchCondition;
-import com.tngtech.archunit.lang.ArchRule;
-import org.apache.commons.lang3.StringUtils;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
 
 /**
  * Created by agarg020917 on 11/17/2017.
  */
 public class ArchUtils {
 
-    private ArchUtils() {
-        throw new UnsupportedOperationException();
-    }
+	private ArchUtils() {
+		throw new UnsupportedOperationException();
+	}
 
-    public static JavaClasses importAllClassesInPackage(String path, String classFolder) {
-        Path classesPath = Paths.get(path + classFolder);
-        if (classesPath.toFile().exists()) {
-            return new ClassFileImporter().importPath(classesPath);
-        }
-        return new ClassFileImporter().importPath(Paths.get(path));
-    }
+	public static JavaClasses importAllClassesInPackage(String path, String classFolder) {
+		Path classesPath = Paths.get(path + classFolder);
+		if (classesPath.toFile().exists()) {
+			return new ClassFileImporter().importPath(classesPath);
+		}
+		return new ClassFileImporter().importPath(Paths.get(path));
+	}
+
+	public static JavaClasses importAllClassesInPackage(String path) {
+		Path classesPath = Paths.get(path);
+		if (classesPath.toFile().exists()) {
+			return new ClassFileImporter().importPath(classesPath);
+		}
+		return new ClassFileImporter().importPath(Paths.get(path));
+	}
 
 }
--- src/test/java/com/societegenerale/commons/plugin/rules/NoPublicFieldRuleTestTest.java
+++ src/test/java/com/societegenerale/commons/plugin/rules/NoPublicFieldRuleTestTest.java
@@ -1,65 +1,26 @@
 package com.societegenerale.commons.plugin.rules;
 
-import static com.societegenerale.commons.plugin.rules.NoPublicFieldRuleTest.NO_PUBLIC_FIELD_VIOLATION_MESSAGE;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.Test;
 
-import com.societegenerale.commons.plugin.rules.classesForTests.ObjectWithNoPublicField;
-import com.societegenerale.commons.plugin.rules.classesForTests.ObjectWithPublicField;
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.lang.ArchRule;
-
 public class NoPublicFieldRuleTestTest {
 
-	@Test
+	String pathObjectWithNoPublicField = "./target/test-classes/com/societegenerale/commons/plugin/rules/classesForTests/ObjectWithNoPublicField.class";
+
+	String pathObjectWithPublicField = "./target/test-classes/com/societegenerale/commons/plugin/rules/classesForTests/ObjectWithPublicField.class";
+
+	@Test(expected = AssertionError.class)
 	public void shouldThrowViolations() {
 
-		assertExceptionIsThrownFor(ObjectWithPublicField.class);
-
-	}
-
-	@Test
-	public void implementingArchRuleTest() {
-
-		JavaClasses importedClass = new ClassFileImporter().importClasses(NoPublicFieldRuleTest.class);
-
-		ArchRule rule = classes().should().implement(ArchRuleTest.class);
-
-		rule.check(importedClass);
+		new NoPublicFieldRuleTest().execute(pathObjectWithPublicField);
 
 	}
 
 	@Test
 	public void shouldNotThrowAnyViolation() {
 
-		assertNoExceptionIsThrownFor(ObjectWithNoPublicField.class);
-
-	}
-
-	private void assertExceptionIsThrownFor(Class clazz) {
-
-		JavaClasses classToTest = new ClassFileImporter().importClasses(clazz);
-
-		assertThatThrownBy(() -> {
-			fields().should(NoPublicFieldRuleTest.notBePublicField()).check(classToTest);
-		}).hasMessageStartingWith("Architecture Violation")
-				.hasMessageContaining("Rule 'fields should not use public field'")
-				.hasMessageContaining("was violated (1 times)").hasMessageContaining(NO_PUBLIC_FIELD_VIOLATION_MESSAGE)
-				.hasMessageContaining(" - class: ").hasMessageContaining(clazz.getName())
-				.hasMessageContaining(" - field name: ");
-
-	}
-
-	private void assertNoExceptionIsThrownFor(Class clazz) {
-
-		JavaClasses classToTest = new ClassFileImporter().importClasses(clazz);
-
-		assertThatCode(() -> fields().should(NoPublicFieldRuleTest.notBePublicField()).check(classToTest))
+		assertThatCode(() -> new NoPublicFieldRuleTest().execute(pathObjectWithNoPublicField))
 				.doesNotThrowAnyException();
 
 	}
