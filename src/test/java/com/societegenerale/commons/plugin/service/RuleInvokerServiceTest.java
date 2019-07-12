2d8d9cf985db09a3a7a75cc6d86c63958bc3af03
Peter Gafert <superpete@gmx.de>
2019-07-12 20:16:21 +0700

    Add validation for checks, support ArchTest methods (#15)
    * Refactored test: Seemed like "defaultPom" and "pomDom" were meanwhile obsolete. Also added a little more structure to assertions -> ExpectedRuleFailure
    
    * Remove strange whitespace from condition description
    
    * Refactoring: Extracted evaluation of custom rules from RuleInvokerService. Classes to evaluate ConfigurableRules are now only imported once.
    
    * Missing checks now fail the invocation of ConfigurableRules
    
    * Plugin can now evaluate ArchRule methods as they are supported by ArchUnit's JUnit support, i.e. methods that take JavaClasses as single parameter. Removed the support for methods returning ArchCondition since the use seems limited (you only save the wrapping with "classes().should(..)").
    
    * Adjusted docs to reflect the new case and added missing test that all checks will be executed if the checks block is missing.

--- src/test/java/com/societegenerale/commons/plugin/service/RuleInvokerServiceTest.java
+++ src/test/java/com/societegenerale/commons/plugin/service/RuleInvokerServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RuleInvokerServiceTest {
@@ -19,7 +20,7 @@ public class RuleInvokerServiceTest {
     @Test
     public void shouldInvokePreConfiguredRulesMethod() {
 
-        String errorMessage = ruleInvokerService.invokePreConfiguredRule(NoStandardStreamRuleTest.class, "./target/test-classes/com/societegenerale/commons/plugin/rules/classesForTests");
+        String errorMessage = ruleInvokerService.invokePreConfiguredRule(NoStandardStreamRuleTest.class.getName(), "./target/test-classes/com/societegenerale/commons/plugin/rules/classesForTests");
 
         assertThat(errorMessage).isNotEmpty();
         assertThat(errorMessage).contains("Architecture Violation");
@@ -27,14 +28,15 @@ public class RuleInvokerServiceTest {
     }
 
     @Test
-    public void shouldExecute2ConfigurableRulesOnTest() throws ReflectiveOperationException {
+    public void shouldExecute2ConfigurableRulesOnTest() {
 
         ApplyOn applyOn = new ApplyOn("com.societegenerale.commons.plugin.rules","test");
 
+        configurableRule.setRule(DummyCustomRule.class.getName());
         configurableRule.setApplyOn(applyOn);
         configurableRule.setChecks(Arrays.asList("annotatedWithTest","resideInMyPackage"));
 
-        String errorMessage = ruleInvokerService.invokeConfigurableRules(DummyCustomRule.class, configurableRule, "./target/test-classes/com/societegenerale/commons/plugin/rules/classesForTests");
+        String errorMessage = ruleInvokerService.invokeConfigurableRules(configurableRule, "./target/test-classes/com/societegenerale/commons/plugin/rules/classesForTests");
         assertThat(errorMessage).isNotEmpty();
         assertThat(errorMessage).contains("Architecture Violation");
         assertThat(errorMessage).contains("classes should be annotated with @Test");
@@ -42,14 +44,15 @@ public class RuleInvokerServiceTest {
     }
 
     @Test
-    public void shouldExecuteOnlyTheConfiguredRule() throws ReflectiveOperationException {
+    public void shouldExecuteOnlyTheConfiguredRule() {
 
         ApplyOn applyOn = new ApplyOn("com.societegenerale.commons.plugin.rules","test");
 
+        configurableRule.setRule(DummyCustomRule.class.getName());
         configurableRule.setApplyOn(applyOn);
-        configurableRule.setChecks(Arrays.asList("annotatedWithTest"));
+        configurableRule.setChecks(singletonList("annotatedWithTest"));
 
-        String errorMessage = ruleInvokerService.invokeConfigurableRules(DummyCustomRule.class, configurableRule, "./target/test-classes/com/societegenerale/commons/plugin/rules/classesForTests");
+        String errorMessage = ruleInvokerService.invokeConfigurableRules(configurableRule, "./target/test-classes/com/societegenerale/commons/plugin/rules/classesForTests");
         assertThat(errorMessage).isNotEmpty();
         assertThat(errorMessage).contains("Architecture Violation");
         assertThat(errorMessage).contains("classes should be annotated with @Test");
@@ -58,15 +61,14 @@ public class RuleInvokerServiceTest {
 
 
     @Test
-    public void shouldExecuteAllRulesFromConfigurableClassByDefault() throws ReflectiveOperationException {
-
-        Class<?> ruleClass = DummyCustomRule.class;
+    public void shouldExecuteAllRulesFromConfigurableClassByDefault() {
 
         ApplyOn applyOn = new ApplyOn("com.societegenerale.commons.plugin.rules","main");
 
+        configurableRule.setRule(DummyCustomRule.class.getName());
         configurableRule.setApplyOn(applyOn);
 
-        String errorMessage = ruleInvokerService.invokeConfigurableRules(ruleClass, configurableRule, "./target/classes/com/societegenerale/commons/plugin/rules");
+        String errorMessage = ruleInvokerService.invokeConfigurableRules(configurableRule, "./target/classes/com/societegenerale/commons/plugin/rules");
 
         assertThat(errorMessage).isNotEmpty();
         assertThat(errorMessage).contains("Architecture Violation");
@@ -75,14 +77,14 @@ public class RuleInvokerServiceTest {
     }
 
     @Test
-    public void shouldExecuteAllRulesOnSpecificPackageInTest() throws ReflectiveOperationException {
-
-        Class<?> ruleClass = DummyCustomRule.class;
+    public void shouldExecuteAllRulesOnSpecificPackageInTest() {
 
         ApplyOn applyOn = new ApplyOn("com.societegenerale.commons.plugin.rules","test");
+
+        configurableRule.setRule(DummyCustomRule.class.getName());
         configurableRule.setApplyOn(applyOn);
 
-        String errorMessage = ruleInvokerService.invokeConfigurableRules(ruleClass, configurableRule, "./target/test-classes/com/societegenerale/commons/plugin/rules/classesForTests/specificCase");
+        String errorMessage = ruleInvokerService.invokeConfigurableRules(configurableRule, "./target/test-classes/com/societegenerale/commons/plugin/rules/classesForTests/specificCase");
         assertThat(errorMessage).isNotEmpty();
         assertThat(errorMessage).contains("Architecture Violation");
         assertThat(errorMessage).contains("Rule 'classes should be annotated with @Test' was violated (1 times)");
