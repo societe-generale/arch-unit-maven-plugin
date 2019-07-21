d5e512b8f534711bb3039ee853a5066671f421b6
Vincent FUCHS <vincent.fuchs@gmail.com>
2019-07-21 18:13:18 +0200

    * added a warning log when package doesn't exist * refactored test

--- src/test/java/com/societegenerale/commons/plugin/rules/ArchUtilsTest.java
+++ src/test/java/com/societegenerale/commons/plugin/rules/ArchUtilsTest.java
@@ -1,54 +1,32 @@
 package com.societegenerale.commons.plugin.rules;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-
-import org.junit.Assert;
+import com.societegenerale.commons.plugin.utils.ArchUtils;
+import com.tngtech.archunit.core.domain.JavaClasses;
 import org.junit.Test;
 
-import com.societegenerale.commons.plugin.utils.ArchUtils;
-import com.tngtech.archunit.core.domain.JavaClass;
-import com.tngtech.archunit.core.domain.JavaClasses;
+import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * These are not great test, as they will fail when new classes are added in the code base, but acceptable for now
+ */
 public class ArchUtilsTest {
 
 	@Test
-	public void constructorInvocationTest() {
-		try {
-			final Constructor<ArchUtils> c = ArchUtils.class.getDeclaredConstructor();
-			c.setAccessible(true);
-			final ArchUtils newInstance = c.newInstance();
-			Assert.assertNull(newInstance);
-		} catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
-				| IllegalArgumentException | InvocationTargetException e) {
-			assertThat(e).hasCauseExactlyInstanceOf(UnsupportedOperationException.class);
-		}
-	}
-
-	@Test
-	public void testNumberOfRulesAvailable() {
+	public void shouldLoadClassesFromGivenPackage() {
 		JavaClasses classes = ArchUtils
-				.importAllClassesInPackage("./target/classes/com/societegenerale/commons/plugin/", "rules");
-		int noOfClasses = 0;
-		for (JavaClass javaClass : classes) {
-			if (!javaClass.getName().contains("$") && !javaClass.isInterface()) {
-				noOfClasses++;
-			}
-		}
-		assertThat(noOfClasses).isEqualTo(10);
+				.importAllClassesInPackage("./target/classes/", "com/societegenerale/commons/plugin/model");
+
+		long noOfClassesInPackage = classes.stream().count();
+
+		assertThat(noOfClassesInPackage).isEqualTo(3);
 	}
 
 	@Test
-	public void totalClassesInPathWhenPackageFolderDoesNotExists() {
-		JavaClasses classes = ArchUtils.importAllClassesInPackage("./target/classes", "classFolder");
-		int noOfClasses = 0;
-		for (JavaClass javaClass : classes) {
-			if (!javaClass.getName().contains("$")) {
-				noOfClasses++;
-			}
-		}
+	public void shouldLoadAllClassesWhenGivenPakageDoesntExist() {
+		JavaClasses classes = ArchUtils.importAllClassesInPackage("./target/classes", "someNotExistingFolder");
+
+		long noOfClasses = classes.stream().filter(it -> !it.isInnerClass()).count();
+
 		assertThat(noOfClasses).isEqualTo(21);
 	}
 
