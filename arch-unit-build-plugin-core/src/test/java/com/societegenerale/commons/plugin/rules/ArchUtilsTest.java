bec32c8e606eb0fcdfef82394dbb26db43b11bbf
Vincent Fuchs <Win-PRD;1908!>
2019-10-03 11:29:45 +0200

    core is now building

--- arch-unit-build-plugin-core/src/test/java/com/societegenerale/commons/plugin/rules/ArchUtilsTest.java
+++ arch-unit-build-plugin-core/src/test/java/com/societegenerale/commons/plugin/rules/ArchUtilsTest.java
@@ -1,5 +1,6 @@
 package com.societegenerale.commons.plugin.rules;
 
+import com.societegenerale.commons.plugin.SilentLog;
 import com.societegenerale.commons.plugin.utils.ArchUtils;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import org.junit.Test;
@@ -10,6 +11,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  * These are not great test, as they will fail when new classes are added in the code base, but acceptable for now
  */
 public class ArchUtilsTest {
+
+	//instantiating to init the static logger in ArchUtils..
+	ArchUtils archUtils=new ArchUtils(new SilentLog());
 
 	@Test
 	public void shouldLoadClassesFromGivenPackage() {
