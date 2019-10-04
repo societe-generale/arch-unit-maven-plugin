b35366b35a6ccdb143da971a7180e0ffeffed5e5
Vincent FUCHS <vincent.fuchs@gmail.com>
2019-10-04 17:23:33 +0200

    removing useless test classes

--- arch-unit-maven-plugin/src/test/java/com/societegenerale/aut/main/ObjectWithAdateField.java
+++
@@ -1,9 +0,0 @@
-package com.societegenerale.aut.main;
-
-import java.util.Date;
-
-public class ObjectWithAdateField {
-
-	private Date date = new Date(0, 0, 0);
-
-}
--- arch-unit-maven-plugin/src/test/java/com/societegenerale/aut/test/TestClassWithStandardStream.java
+++
@@ -1,9 +0,0 @@
-package com.societegenerale.aut.test;
-
-public class TestClassWithStandardStream {
-
-  public static void printOutputToStandardStream() {
-   System.out.println("Print to Console");
-  }
-
-}
