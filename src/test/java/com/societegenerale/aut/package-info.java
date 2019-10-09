e9a1ec239e169ca3bd3c22fd83c7dd84982c0b5e
FanJups <jupsfan@gmail.com>
2019-10-09 04:54:44 +0200

    Changes from com.societegenerale.aut.test to com.societegenerale.aut
    Because:
    - This file belongs to com.societegenerale.aut

--- src/test/java/com/societegenerale/aut/package-info.java
+++ src/test/java/com/societegenerale/aut/package-info.java
@@ -1,10 +1,14 @@
 /**
- * This package contains the AUT (application under test) which the tests run against. These files get compiled via
- * Maven's {@code testCompile} and get afterwards copied to a certain directory.
+ * This package contains the AUT (application under test) which the tests run
+ * against. These files get compiled via Maven's {@code testCompile} and get
+ * afterwards copied to a certain directory.
  * <p>
- * {@link com.societegenerale.aut.main} gets copied to {@code target/aut-target/classes} whereas
- * {@link com.societegenerale.aut.test} gets copied to {@code target/aut-target/test-classes}.
+ * {@link com.societegenerale.aut.main} gets copied to
+ * {@code target/aut-target/classes} whereas
+ * {@link com.societegenerale.aut.test} gets copied to
+ * {@code target/aut-target/test-classes}.
  * <p>
- * This allows the tests to run just against these set of classes and not against themselves, too.
+ * This allows the tests to run just against these set of classes and not
+ * against themselves, too.
  */
-package com.societegenerale.aut.test;
+package com.societegenerale.aut;
