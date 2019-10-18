7f41b53e204f79e91201989c86e60b8966e30007
FanJups <jupsfan@gmail.com>
2019-10-18 21:57:55 +0200

    Method reference
    MavenConfigurableRule::toCoreConfigurableRule

--- src/main/java/com/societegenerale/commons/plugin/maven/model/MavenRules.java
+++ src/main/java/com/societegenerale/commons/plugin/maven/model/MavenRules.java
@@ -1,53 +1,51 @@
 package com.societegenerale.commons.plugin.maven.model;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import com.societegenerale.commons.plugin.model.Rules;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import static java.util.stream.Collectors.toList;
+import com.societegenerale.commons.plugin.model.Rules;
 
 public class MavenRules {
 
-    @Parameter(property = "preConfiguredRules")
-    private List<String> preConfiguredRules= new ArrayList<>();
+	@Parameter(property = "preConfiguredRules")
+	private List<String> preConfiguredRules = new ArrayList<>();
 
-    @Parameter(property = "configurableRules")
-    private List<MavenConfigurableRule> configurableRules = new ArrayList<>();
+	@Parameter(property = "configurableRules")
+	private List<MavenConfigurableRule> configurableRules = new ArrayList<>();
 
-    public MavenRules() {
-        //no arg constructor required by Maven when running the plugin
-    }
+	public MavenRules() {
+		// no arg constructor required by Maven when running the plugin
+	}
 
-    public MavenRules(List<String> preConfiguredRules, List<MavenConfigurableRule> configurableRules) {
-        this.preConfiguredRules = preConfiguredRules;
-        this.configurableRules = configurableRules;
-    }
+	public MavenRules(List<String> preConfiguredRules, List<MavenConfigurableRule> configurableRules) {
+		this.preConfiguredRules = preConfiguredRules;
+		this.configurableRules = configurableRules;
+	}
 
-    public Rules toCoreRules(){
+	public Rules toCoreRules() {
 
-        return new Rules(preConfiguredRules,
-                configurableRules.stream()
-                                 .map(e -> e.toCoreConfigurableRule())
-                                 .collect(toList()));
-    }
+		return new Rules(preConfiguredRules,
+				configurableRules.stream().map(MavenConfigurableRule::toCoreConfigurableRule).collect(toList()));
+	}
 
-    public List<String> getPreConfiguredRules() {
-        return preConfiguredRules;
-    }
+	public List<String> getPreConfiguredRules() {
+		return preConfiguredRules;
+	}
 
-    public List<MavenConfigurableRule> getConfigurableRules() {
-        return configurableRules;
-    }
+	public List<MavenConfigurableRule> getConfigurableRules() {
+		return configurableRules;
+	}
 
-    public void setPreConfiguredRules(List<String> preConfiguredRules) {
-        this.preConfiguredRules = preConfiguredRules;
-    }
+	public void setPreConfiguredRules(List<String> preConfiguredRules) {
+		this.preConfiguredRules = preConfiguredRules;
+	}
 
-    public void setConfigurableRules(List<MavenConfigurableRule> configurableRules) {
-        this.configurableRules = configurableRules;
-    }
-
+	public void setConfigurableRules(List<MavenConfigurableRule> configurableRules) {
+		this.configurableRules = configurableRules;
+	}
 
 }
